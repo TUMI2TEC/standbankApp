7b5c4daeb7c41f4fb84beb5a79db5642809d02a7
TUMI2TEC <93222928+TUMI2TEC@users.noreply.github.com>
2023-06-05 14:16:37 +0200

    Add files via upload

---
+++ loveStandardbank/loveStandardbank/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace loveStandardbank
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.answerBtn = new System.Windows.Forms.Button();
+			this.exitBtn = new System.Windows.Forms.Button();
+			this.restartBtn = new System.Windows.Forms.Button();
+			this.questions = new System.Windows.Forms.TextBox();
+			this.userAnswer = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// answerBtn
+			// 
+			this.answerBtn.BackColor = System.Drawing.Color.Lime;
+			this.answerBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.answerBtn.Location = new System.Drawing.Point(379, 68);
+			this.answerBtn.Name = "answerBtn";
+			this.answerBtn.Size = new System.Drawing.Size(124, 35);
+			this.answerBtn.TabIndex = 0;
+			this.answerBtn.Text = "ANSWER";
+			this.answerBtn.UseVisualStyleBackColor = false;
+			this.answerBtn.Click += new System.EventHandler(this.answerBtn_Click);
+			// 
+			// exitBtn
+			// 
+			this.exitBtn.BackColor = System.Drawing.Color.Red;
+			this.exitBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.exitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.exitBtn.Location = new System.Drawing.Point(379, 227);
+			this.exitBtn.Name = "exitBtn";
+			this.exitBtn.Size = new System.Drawing.Size(124, 35);
+			this.exitBtn.TabIndex = 1;
+			this.exitBtn.Text = "EXIT";
+			this.exitBtn.UseVisualStyleBackColor = false;
+			this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+			// 
+			// restartBtn
+			// 
+			this.restartBtn.BackColor = System.Drawing.Color.Yellow;
+			this.restartBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.restartBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.restartBtn.Location = new System.Drawing.Point(379, 146);
+			this.restartBtn.Name = "restartBtn";
+			this.restartBtn.Size = new System.Drawing.Size(124, 35);
+			this.restartBtn.TabIndex = 2;
+			this.restartBtn.Text = "RESTART";
+			this.restartBtn.UseVisualStyleBackColor = false;
+			this.restartBtn.Click += new System.EventHandler(this.restartBtn_Click);
+			// 
+			// questions
+			// 
+			this.questions.Location = new System.Drawing.Point(106, 68);
+			this.questions.Name = "questions";
+			this.questions.Size = new System.Drawing.Size(137, 20);
+			this.questions.TabIndex = 3;
+			// 
+			// userAnswer
+			// 
+			this.userAnswer.Location = new System.Drawing.Point(106, 126);
+			this.userAnswer.Name = "userAnswer";
+			this.userAnswer.Size = new System.Drawing.Size(137, 20);
+			this.userAnswer.TabIndex = 4;
+			// 
+			// Form1
+			// 
+			this.AcceptButton = this.answerBtn;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.exitBtn;
+			this.ClientSize = new System.Drawing.Size(637, 291);
+			this.Controls.Add(this.userAnswer);
+			this.Controls.Add(this.questions);
+			this.Controls.Add(this.restartBtn);
+			this.Controls.Add(this.exitBtn);
+			this.Controls.Add(this.answerBtn);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button answerBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button restartBtn;
+        private System.Windows.Forms.TextBox questions;
+        private System.Windows.Forms.TextBox userAnswer;
+    }
+}
+
---
+++ loveStandardbank/loveStandardbank/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loveStandardbank
+{
+    public partial class Form1 : Form
+    {
+        private Random random;
+        private int num1;
+        private int num2;
+        private int answer;
+        private int num3;
+        private int num4;
+        private int score;
+        private int totalQuestions;
+        private int length;
+		private int count;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+            score = 0;
+            totalQuestions = 0;
+            length = 10;
+			count = 0;
+
+        }
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			GenerateQuestion();
+		}
+
+
+		private void GenerateQuestion() //function for generating number and quetions
+        {
+			for (count= 0; count <= length; count++) //for-loop for monitoring the numer of question
+			{
+
+				num1 = random.Next(1, 51); // Generates a number between 1 and 50
+				num2 = random.Next(1, 51);
+
+				//randomisation for multipucation
+
+				num3 = random.Next(1, 13);  // Generates a number between 1 and 12
+				num4 = random.Next(1, 13);
+
+				int operation = random.Next(0, 4); //randomizing operators
+
+				switch (operation)  // using switch case method to choose the equations 
+				{
+					case 0:
+						answer = num1 + num2;
+						questions.Text = $"{num1} + {num2} =";
+						break;
+					case 1:
+						answer = num1 - num2;
+						questions.Text = $"{num1} - {num2} =";
+						break;
+					case 2:
+
+						answer = num1 * num2;
+						questions.Text = $"{num3} * {num4} =";
+
+						break;
+					case 3:
+						answer = num1 / num2;
+						questions.Text = $"{num1} / {num2} =";
+						break;
+				}
+
+
+				// focusing the cursor 
+
+				userAnswer.Text = "";
+				userAnswer.Focus();
+			}
+         }
+
+        private void answerBtn_Click(object sender, EventArgs e)
+        {
+			// it checks the answer
+            if (int.TryParse(userAnswer.Text, out int userResponse))
+            {
+                totalQuestions++;
+                if (userResponse == answer)
+                {
+                    score++;
+                    MessageBox.Show("Correct answer!");
+                }
+                else
+                {
+                    MessageBox.Show($"Wrong answer! The correct answer is {answer}.");
+
+
+                }
+                GenerateQuestion();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid integer.");
+            }
+
+			if (totalQuestions==length)
+			{
+				MessageBox.Show($"You have Scored : {score}/{totalQuestions}");
+				answerBtn.Enabled = false;
+				questions.Enabled = false;
+				userAnswer.Enabled = false;
+
+
+			}
+
+        }
+
+        
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+			Application.Exit();
+        }
+
+		private void restartBtn_Click(object sender, EventArgs e)
+		{
+			answerBtn.Enabled = true;
+			questions.Enabled = true;
+			userAnswer.Enabled = true;
+			GenerateQuestion();
+
+		}
+	}
+
+    
+}   
